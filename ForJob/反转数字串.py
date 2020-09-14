1e2615559dc44b8c3b1da7fd27eb111d914a5d76
unknown <YCKJ3529@ad.yc>
2020-09-14 10:41:31 +0800

    update

--- ForJob/反转数字串.py
+++
@@ -1,7 +0,0 @@
-import sys
-if __name__ == "__main__":
-    line = sys.stdin.readline().strip()
-    values = list(map(int, line.split()))
-    b=reversed(values)
-    for i in b:
-        print(i,end=" ")
